dd072e7683fca883036a80ffd45bbd1eee700e78
yannxiao <striverx@163.com>
2016-03-03 15:35:53 +0800

    Update

--- Examples/Basic/index.ios.js
+++ Examples/Basic/index.ios.js
@@ -1,102 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
-import React, {
-    View, Text, StyleSheet,
-} from 'react-native';
-
-import Keyboard from 'react-native-keyboard';
+import React from 'react-native';
+import App from './app';
+React.AppRegistry.registerComponent('keyboard', () => App);
 
 
-let model = {
-    
-    _keys: [],
-
-    _listeners: [],
-
-    addKey(key) {
-        this._keys.push(key);
-        this._notify();
-    },
-
-    delKey() {
-        console.log(123)
-        this._keys.pop();
-        this._notify();
-    },
-
-    clearAll() {
-        this._keys = [];
-        this._notify();
-    },
-
-    getKeys() {
-        return this._keys;
-    },
-
-    onChange(listener) {
-        if (typeof listener === 'function') {
-            this._listeners.push(listener);
-        }
-    },
-
-    _notify() {
-        this._listeners.forEach((listner) => {
-            listner(this);
-        });
-    }
-};
 
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: ''
-        };
-    }
-
-    componentDidMount() {
-        model.onChange((model) => {
-            this.setState({text: model.getKeys().join('')});
-        });
-    }
-
-    _handleClear() {
-        model.clearAll();
-    }
-
-    _handleDelete() {
-        model.delKey();
-    }
-
-    _handleKeyPress(key) {
-        model.addKey(key);
-    }
-
-    render() {
-        return (
-            <View style={{flex: 1}}>
-                <Text style={styles.text}>{this.state.text}</Text>
-
-                <Keyboard 
-                    isRenderDot={true}
-                    onClear={this._handleClear.bind(this)}
-                    onDelete={this._handleDelete.bind(this)}
-                    onKeyPress={this._handleKeyPress.bind(this)}
-                />
-            </View>
-        );
-    }
-}
-
-let styles = StyleSheet.create({
-    text: {marginTop: 100, textAlign: 'center'}
-});
-
-
-React.AppRegistry.registerComponent('Basic', () => App);
 
